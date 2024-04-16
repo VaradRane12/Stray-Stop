0700cbee9863c4e80c77f81dfef53d1670ae4fef
Varad Rane <varadjrane@gmail.com>
2024-04-16 23:38:05 +0530

    qr adding

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,12 @@
 function openForm() {
     document.getElementById("myForm").style.display = "block";
   }
-  
+  function openForm1() {
+    document.getElementById("myForm1").style.display = "block";
+  }
+  function closeForm1() {
+    document.getElementById("myForm1").style.display = "none";
+  }
   function closeForm() {
     document.getElementById("myForm").style.display = "none";
   }
